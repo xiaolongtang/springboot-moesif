ee4e5bfb7f5cc47a339c4e12e025a71c517383e7
晓龙 唐 <xiaolongtang@xiaolongdeMacBook-Pro.local>
2019-09-01 21:55:15 +0800

    update the controller

--- src/main/java/com/fil/moesif/web/WebController.java
+++ src/main/java/com/fil/moesif/web/WebController.java
@@ -1,10 +1,11 @@
 package com.fil.moesif.web;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
 
 @CrossOrigin
 @RestController
@@ -16,8 +17,32 @@ public class WebController {
         return "{\"msg\":\"hello world\"}";
     }
 
+    @RequestMapping(value = "/code/{code}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Object errorAPI(@PathVariable int code){
+        switch(code){
+            case 404 :
+                throw new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "404 not found"
+                );
+            case 500 :
+                throw new ResponseStatusException(
+                        HttpStatus.SERVICE_UNAVAILABLE, "500 "
+                );
+            case 401 :
+                throw new ResponseStatusException(
+                        HttpStatus.UNAUTHORIZED, "401"
+                );
+            case 403 :
+                throw new ResponseStatusException(
+                        HttpStatus.FORBIDDEN, "403"
+                );
+                default:
+                    return "{\"msg\":\"hello world\"}";
+        }
+    }
+
     @RequestMapping(value = "/error", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Object errorAPI(){
-        throw new NullPointerException();
+    public Object errorException(){
+        throw new ArrayIndexOutOfBoundsException();
     }
 }
